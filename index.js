8aa82a0317e73082e33abeaa076a1ee544799a9c
asqararslonov <arslonovasqar123@gmail.com>
2023-03-22 14:21:29 +0500

    feat: fixed cors2

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const app = express()
 
 
 // Middlewares
-app.use(cors({
-    origin: ['https://www.leksika.uz', 'http://localhost:3000', 'https://crud-html.vercel.app/']
-}))
+// app.use(cors({
+//     origin: ['https://www.leksika.uz', 'http://localhost:3000', 'https://crud-html.vercel.app/']
+// }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
